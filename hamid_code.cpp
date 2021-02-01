41eeababb3eb75a6f5c769de78bd6bfc0059567f
hamid-ali <40883267+hamid-ali@users.noreply.github.com>
2021-02-01 10:13:38 +0530

    Update hamid_code.cpp
    Removing all if conditions

--- hamid_code.cpp
+++ hamid_code.cpp
@@ -4,21 +4,23 @@ using namespace std;
 
 void print(int n) 
 { 
+    bool flag = false;
     int x = 0;
     for (int i = 0; i < n; i++) { 
         for(int k = 0 ; k< n-i-1 ; k++)
             cout<<" ";
-        if (i % 2 == 0) { 
-            for (int j = 1; j <= n; j++) 
+        // if (i % 2 == 0) { 
+            for (int j = 1; j <= n && !flag; j++) 
                 cout << j+x ; 
   
-        } 
-        else { 
-            for (int j = n; j > 0; j--) 
+        // } 
+        // else { 
+            for (int j = n; j > 0 &&  flag; j--) 
                 cout <<j+x ; 
-        }
+        // }
         cout<<endl; 
         x = x+n;
+        flag = !flag ;
     } 
 }  
 
